c352558ef729a41e6b68267506eb1490a4dea2c9
Thales Miguel <thales.miguel@gmail.com>
2016-10-27 08:59:28 -0200

    Ajustes de Layout

--- app/assets/javascripts/eventos.js
+++ app/assets/javascripts/eventos.js
@@ -20,6 +20,7 @@ $(document).on('turbolinks:load', function() {
       listWeek: { titleFormat: "DD/MM/YYYY", listDayFormat: "dddd, D/M/Y", listDayAltFormat: false }},
     eventRender: function(event, element, view) {
       element.attr("data-remote",true);
+      element.find("a").attr("data-remote",true);
       // console.log(event)
       if (event.aprovado == true) {element.find(".fc-content").append("<i class='material-icons fc-icon'>check</i>")};
 
